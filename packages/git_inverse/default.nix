68df0a83b8844cd4f34ea68d263985875cd4c060
Paschalis Bizopoulos <pbizop@gmail.com>
2026-03-03 15:16:27 +0200

    .

--- packages/git_inverse/default.nix
+++ packages/git_inverse/default.nix
@@ -1,21 +1,23 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "git_inverse";
-  version = "0.1.0";
-  src = ./.;
-  nativeBuildInputs = [
-    pkgs.makeWrapper
-  ];
   buildInputs = [
     pkgs.git
     pkgs.imagemagick
   ];
+  cargoHash = "sha256-Rn7HzfOe4DH/dLlSDHQeHIcHOiqPzjLTRKnEObV6RZk=";
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+  pname = "git_inverse";
   postInstall = ''
     wrapProgram $out/bin/git_inverse \
-      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.git pkgs.imagemagick ]}
+      --prefix PATH : ${
+        pkgs.lib.makeBinPath [
+          pkgs.git
+          pkgs.imagemagick
+        ]
+      }
   '';
-  cargoHash = "sha256-Rn7HzfOe4DH/dLlSDHQeHIcHOiqPzjLTRKnEObV6RZk=";
+  src = ./.;
+  version = "0.1.0";
 }
