e7751d3ecce7c2c287ec899c933cf75661d525df
Paschalis Bizopoulos <pbizop@gmail.com>
2026-03-04 14:13:06 +0200

    .

---
+++ templates/packages/packages/html/default.nix
@@ -0,0 +1,34 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  pname = "html";
+in
+pkgs.stdenv.mkDerivation {
+  inherit pname;
+  buildInputs = [ pkgs.bash ];
+  installPhase = ''
+        mkdir -p $out/share/doc
+        cp -f ${./.}/* $out/share/doc/
+        mkdir -p $out/bin
+        cat <<EOF > $out/bin/${pname}
+    #!/usr/bin/env bash
+    if [ "\$DEBUG" == "1" ]; then
+      for f in index.html style.css script.js; do
+        if [ -f $out/share/doc/\$f ]; then
+          echo "test \$f ... ok"
+        else
+          echo "test \$f ... failed"
+          exit 1
+        fi
+      done
+    else
+      ${pkgs.nodePackages.http-server}/bin/http-server $out/share/doc
+    fi
+    EOF
+        chmod +555 $out/bin/${pname}
+  '';
+  phases = [ "installPhase" ];
+  src = ./.;
+  version = "0.0.0";
+}
