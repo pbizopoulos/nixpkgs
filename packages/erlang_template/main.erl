4d03a71dd8b0c7461cdf959d01c7b4eeccb856de
Paschalis Bizopoulos <pbizop@gmail.com>
2026-03-04 21:13:39 +0200

    .

--- packages/erlang_template/main.erl
+++ packages/erlang_template/main.erl
@@ -1,16 +1,16 @@
 #!/usr/bin/env escript
 main(_) ->
-  case os:getenv("DEBUG") of
-    "1" ->
-      run_tests();
-    _ ->
-      io:format("Hello Erlang!~n")
-  end.
+    case os:getenv("DEBUG") of
+        "1" ->
+            run_tests();
+        _ ->
+            io:format("Hello Erlang!~n")
+    end.
 run_tests() ->
-  case 1 + 1 of
-    2 ->
-      io:format("test math ... ok~n");
-    _ ->
-      io:format("test math failed~n"),
-      halt(1)
-  end.
+    case 1 + 1 of
+        2 ->
+            io:format("test math ... ok~n");
+        _ ->
+            io:format("test math failed~n"),
+            halt(1)
+    end.
