6e69573400a828f050ec5d4f9b0a3bf94615e8ac
Paschalis Bizopoulos <pbizop@gmail.com>
2026-03-11 21:03:19 +0200

    .

--- packages/nextjs_template/default.nix
+++ packages/nextjs_template/default.nix
@@ -1,17 +1,12 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.buildNpmPackage rec {
+pkgs.buildNpmPackage {
   nativeBuildInputs = [
-    pkgs.makeWrapper
+    pkgs.supabase-cli
     pkgs.openssl
-    pkgs.supabase-cli
   ];
-  buildInputs = [
-    pkgs.nodejs
-    pkgs.supabase-cli
-  ];
-  npmDepsHash = "sha256-PYHvoR6lfkMxP4m4ku7LhXn4KNwVDCGZ2njBrTSC3BE=";
+  npmDepsHash = "sha256-icIIG7nHct4SYZubYOwc37RtwcTgW/0CYCU4LTO9iv4=";
   pname = baseNameOf ./.;
   env = {
     NEXT_PUBLIC_SUPABASE_URL = "http://localhost:54321";
@@ -31,20 +26,4 @@ pkgs.buildNpmPackage rec {
   '';
   src = ./.;
   version = "0.0.0";
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/lib/node_modules/${pname}
-    cp -r . $out/lib/node_modules/${pname}
-    mkdir -p $out/bin
-    wrapProgram $out/bin/${pname} \
-      --set PLAYWRIGHT_BROWSERS_PATH ${pkgs.playwright-driver.browsers} \
-      --set PKG_CONFIG_PATH ${pkgs.openssl.dev}/lib/pkgconfig \
-      --prefix PATH : ${
-        pkgs.lib.makeBinPath [
-          pkgs.nodejs
-          pkgs.supabase-cli
-        ]
-      }
-    runHook postInstall
-  '';
 }
--- packages/nextjs_template/scripts/start.js
+++ packages/nextjs_template/scripts/start.js
@@ -1,42 +1,15 @@
 #!/usr/bin/env node
 import { spawn } from "node:child_process";
-import { existsSync } from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const nextBin = join(__dirname, "../node_modules/.bin/next");
 
-if (process.env.DEBUG === "1") {
-  let workingDir = process.cwd();
-  if (existsSync(join(workingDir, "packages/default/package.json"))) {
-    workingDir = join(workingDir, "packages/default");
-  }
-  const test = spawn(
-    "npm install && npm run build && npm run db:stop && npm run db:start && npm test",
-    [],
-    {
-      stdio: "inherit",
-      cwd: workingDir,
-      shell: true,
-      env: {
-        ...process.env,
-        NEXT_PUBLIC_SUPABASE_URL: "http://localhost:54321",
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: "build-placeholder",
-      },
-    },
-  );
-  test.on("close", (code) => {
-    process.exit(code || 0);
-  });
-} else {
-  const workingDir = join(__dirname, "..");
-  const next = spawn(nextBin, ["start"], {
-    stdio: "inherit",
-    cwd: workingDir,
-  });
+const next = spawn(nextBin, ["start"], {
+  stdio: "inherit",
+});
 
-  next.on("close", (code) => {
-    process.exit(code || 0);
-  });
-}
+next.on("close", (code) => {
+  process.exit(code || 0);
+});
