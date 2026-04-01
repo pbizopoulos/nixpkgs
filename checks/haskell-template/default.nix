ddebd782c66960c2a1ae6c1e52cb11501f8130b8
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-01 16:59:26 +0300

    .

--- checks/haskell-template/default.nix
+++ checks/haskell-template/default.nix
@@ -7,7 +7,6 @@ pkgs.testers.runNixOSTest rec {
   name = builtins.baseNameOf ./.;
   nodes.machine.environment.systemPackages = [
     inputs.self.packages.${pkgs.stdenv.system}.${name}
-    pkgs.git
   ];
   testScript = ''
     machine.succeed("DEBUG=1 ${name}")
--- checks/python_template/default.nix
+++ checks/python_template/default.nix
@@ -3,17 +3,10 @@
   pkgs,
   ...
 }:
-let
-  python-with-audit = pkgs.python313.withPackages (ps: [
-    ps.scalene
-  ]);
-in
 pkgs.testers.runNixOSTest rec {
   name = "python_template";
   nodes.machine.environment.systemPackages = [
     inputs.self.packages.${pkgs.stdenv.system}.${name}
-    pkgs.git
-    python-with-audit
   ];
   testScript = ''
     machine.succeed("DEBUG=1 ${name}")
