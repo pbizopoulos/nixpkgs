bcfab237cc350d90e12c8dfdb6030cac8a48b79f
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-22 13:18:33 +0300

    .

--- checks/python_latex_template/default.nix
+++ checks/python_latex_template/default.nix
@@ -3,12 +3,29 @@
   pkgs,
   ...
 }:
-pkgs.testers.runNixOSTest rec {
-  name = "python_latex_template";
-  nodes.machine.environment.systemPackages = [
-    inputs.self.packages.${pkgs.stdenv.system}.${name}
-  ];
-  testScript = ''
-    machine.succeed("DEBUG=1 ${name}")
-  '';
-}
+let
+  name = builtins.baseNameOf ./.;
+  package = inputs.self.packages.${pkgs.stdenv.system}.${name};
+in
+pkgs.runCommand "${name}"
+  {
+    nativeBuildInputs = [
+      (pkgs.python313.withPackages (
+        ps:
+        package.propagatedBuildInputs
+        ++ [
+          ps.coverage
+        ]
+      ))
+      pkgs.texliveFull
+    ];
+    src = ../../packages/${name};
+  }
+  ''
+    export HOME="$PWD"
+    export PYTHON_LATEX_TEMPLATE_ASSETS="$src"
+    DEBUG=1 coverage run --source="$src" "$src/main.py"
+    coverage_percent=$(coverage report | awk '/^TOTAL/{print $4}')
+    echo "coverage: $coverage_percent"
+    touch "$out"
+  ''
