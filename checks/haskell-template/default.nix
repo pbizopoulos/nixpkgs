5d5f4c4d0044994a979a8388e303c53a1b8de4a0
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-22 17:15:00 +0300

    .

--- checks/haskell-template/default.nix
+++ checks/haskell-template/default.nix
@@ -1,14 +1,47 @@
 {
-  inputs,
   pkgs,
   ...
 }:
-pkgs.testers.runNixOSTest rec {
+let
   name = builtins.baseNameOf ./.;
-  nodes.machine.environment.systemPackages = [
-    inputs.self.packages.${pkgs.stdenv.system}.${name}
-  ];
-  testScript = ''
-    machine.succeed("DEBUG=1 ${name}")
-  '';
-}
+  debugGhc = pkgs.haskellPackages.ghcWithPackages (ps: [
+    ps.HUnit
+    ps.aeson
+    ps.bytestring
+  ]);
+in
+pkgs.runCommand "${name}"
+  {
+    nativeBuildInputs = [
+      debugGhc
+      pkgs.coreutils
+    ];
+    src = ../../packages/${name};
+  }
+  ''
+    build_dir="$PWD"
+    workspace="$build_dir/workspace"
+    coverage_dir="$build_dir/coverage"
+    hpcdir="$build_dir/hpc"
+    export HOME="$build_dir"
+    rm -rf "$workspace" "$coverage_dir" "$hpcdir"
+    cp -R "$src" "$workspace"
+    chmod -R u+w "$workspace"
+    mkdir -p "$coverage_dir/html" "$hpcdir"
+    cd "$workspace"
+    "${debugGhc}/bin/ghc" \
+      -fhpc \
+      -hpcdir "$hpcdir" \
+      -outputdir "$build_dir" \
+      -odir "$build_dir" \
+      -hidir "$build_dir" \
+      -o "$build_dir/$name" \
+      Main.hs
+    HPCTIXFILE="$coverage_dir/$name.tix" DEBUG=1 "$build_dir/$name"
+    "${debugGhc}/bin/hpc" markup "$coverage_dir/$name.tix" \
+      --hpcdir="$hpcdir" \
+      --destdir="$coverage_dir/html"
+    "${debugGhc}/bin/hpc" report "$coverage_dir/$name.tix" \
+      --hpcdir="$hpcdir" | tee "$coverage_dir/summary.txt"
+    touch "$out"
+  ''
--- checks/nix-alphabetize/default.nix
+++ checks/nix-alphabetize/default.nix
@@ -1,14 +1,47 @@
 {
-  inputs,
   pkgs,
   ...
 }:
-pkgs.testers.runNixOSTest rec {
+let
   name = builtins.baseNameOf ./.;
-  nodes.machine.environment.systemPackages = [
-    inputs.self.packages.${pkgs.stdenv.system}.${name}
-  ];
-  testScript = ''
-    machine.succeed("DEBUG=1 ${name}")
-  '';
-}
+  debugGhc = pkgs.haskellPackages.ghcWithPackages (ps: [
+    ps.HUnit
+    ps.aeson
+    ps.base
+    ps.bytestring
+    ps.hnix
+    ps.temporary
+    ps.text
+  ]);
+in
+pkgs.runCommand "${name}"
+  {
+    nativeBuildInputs = [
+      debugGhc
+      pkgs.coreutils
+    ];
+    src = ../../packages/${name};
+  }
+  ''
+    build_dir="$PWD"
+    coverage_dir="$build_dir/coverage"
+    hpcdir="$build_dir/hpc"
+    export HOME="$build_dir"
+    rm -rf "$coverage_dir" "$hpcdir"
+    mkdir -p "$coverage_dir/html" "$hpcdir"
+    "${debugGhc}/bin/ghc" \
+      -fhpc \
+      -hpcdir "$hpcdir" \
+      -outputdir "$build_dir" \
+      -odir "$build_dir" \
+      -hidir "$build_dir" \
+      -o "$build_dir/$name" \
+      "$src/Main.hs"
+    HPCTIXFILE="$coverage_dir/$name.tix" DEBUG=1 "$build_dir/$name"
+    "${debugGhc}/bin/hpc" markup "$coverage_dir/$name.tix" \
+      --hpcdir="$hpcdir" \
+      --destdir="$coverage_dir/html"
+    "${debugGhc}/bin/hpc" report "$coverage_dir/$name.tix" \
+      --hpcdir="$hpcdir" | tee "$coverage_dir/summary.txt"
+    touch "$out"
+  ''
