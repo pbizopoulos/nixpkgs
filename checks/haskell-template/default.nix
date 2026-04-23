1d75cb56db40ce10322c3e3b3e4e4be33b8570b0
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-23 19:02:12 +0300

    .

--- checks/haskell-template/default.nix
+++ checks/haskell-template/default.nix
@@ -1,47 +1,17 @@
 {
+  inputs,
   pkgs,
   ...
 }:
 let
-  checkName = builtins.baseNameOf ./.;
-  packageName = "haskell-template";
-  debugGhc = pkgs.haskellPackages.ghcWithPackages (ps: [
-    ps.HUnit
-    ps.aeson
-    ps.base
-    ps.bytestring
-  ]);
+  name = builtins.baseNameOf ./.;
 in
-pkgs.runCommand "${checkName}"
-  {
-    nativeBuildInputs = [
-      debugGhc
-      pkgs.coreutils
-    ];
-    src = ../../packages/${packageName};
-  }
-  ''
-    coverage_dir="$PWD/coverage"
-    hpcdir="$PWD/hpc"
-    export HOME="$PWD"
-    packageName="${packageName}"
-    rm -rf "$PWD/workspace" "$coverage_dir" "$hpcdir"
-    mkdir -p "$PWD/workspace" "$coverage_dir/html" "$hpcdir"
-    cp -R --no-preserve=mode "$src"/. "$PWD/workspace"
-    cd "$PWD/workspace"
-    "${debugGhc}/bin/ghc" \
-      -fhpc \
-      -hpcdir "$hpcdir" \
-      -outputdir "$PWD" \
-      -odir "$PWD" \
-      -hidir "$PWD" \
-      -o "$PWD/$packageName" \
-      Main.hs
-    HPCTIXFILE="$coverage_dir/$packageName.tix" DEBUG=1 "$PWD/$packageName"
-    "${debugGhc}/bin/hpc" markup "$coverage_dir/$packageName.tix" \
-      --hpcdir="$hpcdir" \
-      --destdir="$coverage_dir/html"
-    "${debugGhc}/bin/hpc" report "$coverage_dir/$packageName.tix" \
-      --hpcdir="$hpcdir" | tee "$coverage_dir/summary.txt"
-    touch "$out"
-  ''
+pkgs.testers.runNixOSTest rec {
+  inherit name;
+  nodes.machine.environment.systemPackages = [
+    inputs.self.packages.${pkgs.stdenv.system}.${name}
+  ];
+  testScript = ''
+    machine.succeed("DEBUG=1 ${name}")
+  '';
+}
--- checks/nix-alphabetize/default.nix
+++ checks/nix-alphabetize/default.nix
@@ -1,50 +1,17 @@
 {
+  inputs,
   pkgs,
   ...
 }:
 let
-  checkName = builtins.baseNameOf ./.;
-  packageName = "nix-alphabetize";
-  debugGhc = pkgs.haskellPackages.ghcWithPackages (ps: [
-    ps.HUnit
-    ps.aeson
-    ps.base
-    ps.bytestring
-    ps.data-fix
-    ps.hnix
-    ps.prettyprinter
-    ps.temporary
-    ps.text
-  ]);
+  name = builtins.baseNameOf ./.;
 in
-pkgs.runCommand "${checkName}"
-  {
-    nativeBuildInputs = [
-      debugGhc
-      pkgs.coreutils
-    ];
-    src = ../../packages/${packageName};
-  }
-  ''
-    coverage_dir="$PWD/coverage"
-    hpcdir="$PWD/hpc"
-    export HOME="$PWD"
-    packageName="${packageName}"
-    rm -rf "$coverage_dir" "$hpcdir"
-    mkdir -p "$coverage_dir/html" "$hpcdir"
-    "${debugGhc}/bin/ghc" \
-      -fhpc \
-      -hpcdir "$hpcdir" \
-      -outputdir "$PWD" \
-      -odir "$PWD" \
-      -hidir "$PWD" \
-      -o "$PWD/$packageName" \
-      "$src/Main.hs"
-    HPCTIXFILE="$coverage_dir/$packageName.tix" DEBUG=1 "$PWD/$packageName"
-    "${debugGhc}/bin/hpc" markup "$coverage_dir/$packageName.tix" \
-      --hpcdir="$hpcdir" \
-      --destdir="$coverage_dir/html"
-    "${debugGhc}/bin/hpc" report "$coverage_dir/$packageName.tix" \
-      --hpcdir="$hpcdir" | tee "$coverage_dir/summary.txt"
-    touch "$out"
-  ''
+pkgs.testers.runNixOSTest rec {
+  inherit name;
+  nodes.machine.environment.systemPackages = [
+    inputs.self.packages.${pkgs.stdenv.system}.${name}
+  ];
+  testScript = ''
+    machine.succeed("DEBUG=1 ${name}")
+  '';
+}
