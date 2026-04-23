7121448ac82ebdc11fc018d7a7168b7e8d5b18fe
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-23 12:38:16 +0300

    .

--- checks/python_template/default.nix
+++ checks/python_template/default.nix
@@ -5,45 +5,41 @@
 }:
 let
   name = builtins.baseNameOf ./.;
-  package = inputs.self.packages.${pkgs.stdenv.system}.${name};
-  cosmicRay = inputs.self.packages.${pkgs.stdenv.system}.cosmic_ray;
 in
 pkgs.runCommand "${name}"
   {
     nativeBuildInputs = [
       (pkgs.python312.withPackages (
         _:
-        package.propagatedBuildInputs
+        inputs.self.packages.${pkgs.stdenv.system}.${name}.propagatedBuildInputs
         ++ [
           pkgs.python312Packages.coverage
         ]
       ))
-      cosmicRay
+      inputs.self.packages.${pkgs.stdenv.system}.cosmic_ray
     ];
     src = ../../packages/${name};
   }
   ''
-    export HOME="$PWD"
-    workspace="$PWD/workspace"
-    config_file="$workspace/cosmic-ray.toml"
-    session_file="$workspace/cosmic-ray.sqlite"
-    rm -rf "$workspace"
-    mkdir -p "$workspace"
-    cp -r "$src"/. "$workspace"
-    cd "$workspace"
-    DEBUG=1 coverage run --source=. main.py
-    coverage report
-    cat > "$config_file" <<'EOF'
-        [cosmic-ray]
-        module-path = "main.py"
-        timeout = 10.0
-        excluded-modules = []
-        test-command = "DEBUG=1 python3 main.py"
-        [cosmic-ray.distributor]
-        name = "local"
+        export HOME="$PWD"
+        workspace="$PWD/workspace"
+        rm -rf "$workspace"
+        mkdir -p "$workspace"
+        cp -r "$src"/. "$workspace"
+        cd "$workspace"
+        DEBUG=1 coverage run --source=. main.py
+        coverage report
+        cat > cosmic-ray.toml <<'EOF'
+    [cosmic-ray]
+    module-path = "main.py"
+    timeout = 10.0
+    excluded-modules = []
+    test-command = "DEBUG=1 python3 main.py"
+    [cosmic-ray.distributor]
+    name = "local"
     EOF
-    cosmic-ray init "$config_file" "$session_file"
-    cosmic-ray exec "$config_file" "$session_file"
-    cr-report "$session_file"
-    touch "$out"
+        cosmic-ray init cosmic-ray.toml cosmic-ray.sqlite
+        cosmic-ray exec cosmic-ray.toml cosmic-ray.sqlite
+        cr-report cosmic-ray.sqlite
+        touch "$out"
   ''
