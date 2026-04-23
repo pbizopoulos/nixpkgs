1bd8e1ffc73679ed91bddd8762602fb49ff7a6f7
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-23 17:05:18 +0300

    .

---
+++ checks/remove_empty_lines_coverage/default.nix
@@ -0,0 +1,32 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  checkName = builtins.baseNameOf ./.;
+  packageName = "remove_empty_lines";
+  inherit (inputs.self.packages.${pkgs.stdenv.system}.${packageName}) cargoDeps;
+in
+pkgs.runCommand "${checkName}"
+  {
+    nativeBuildInputs = [
+      pkgs.cargo
+      pkgs.cargo-llvm-cov
+      pkgs.llvmPackages.llvm
+      pkgs.rustc
+      pkgs.stdenv.cc
+    ];
+    src = ../../packages/${packageName};
+  }
+  ''
+    export LLVM_COV='${pkgs.lib.getExe' pkgs.llvmPackages.llvm "llvm-cov"}'
+    export LLVM_PROFDATA='${pkgs.lib.getExe' pkgs.llvmPackages.llvm "llvm-profdata"}'
+    cp -R --no-preserve=mode "$src" "$PWD/workspace"
+    install -Dm644 "${cargoDeps}/.cargo/config.toml" "$PWD/workspace/.cargo/config.toml"
+    substituteInPlace "$PWD/workspace/.cargo/config.toml" \
+      --replace-fail "@vendor@" "${cargoDeps}"
+    cd "$PWD/workspace"
+    cargo llvm-cov
+    touch "$out"
+  ''
--- checks/remove_empty_lines_mutation_testing/default.nix
+++ checks/remove_empty_lines_mutation_testing/default.nix
@@ -4,30 +4,26 @@
   ...
 }:
 let
-  name = builtins.baseNameOf ./.;
-  inherit (inputs.self.packages.${pkgs.stdenv.system}.${name}) cargoDeps;
+  checkName = builtins.baseNameOf ./.;
+  packageName = "remove_empty_lines";
+  inherit (inputs.self.packages.${pkgs.stdenv.system}.${packageName}) cargoDeps;
 in
-pkgs.runCommand "${name}"
+pkgs.runCommand "${checkName}"
   {
     nativeBuildInputs = [
       pkgs.cargo
-      pkgs.cargo-llvm-cov
       pkgs.cargo-mutants
-      pkgs.llvmPackages.llvm
       pkgs.rustc
       pkgs.stdenv.cc
     ];
-    src = ../../packages/${name};
+    src = ../../packages/${packageName};
   }
   ''
-    export LLVM_COV='${pkgs.lib.getExe' pkgs.llvmPackages.llvm "llvm-cov"}'
-    export LLVM_PROFDATA='${pkgs.lib.getExe' pkgs.llvmPackages.llvm "llvm-profdata"}'
     cp -R --no-preserve=mode "$src" "$PWD/workspace"
     install -Dm644 "${cargoDeps}/.cargo/config.toml" "$PWD/workspace/.cargo/config.toml"
     substituteInPlace "$PWD/workspace/.cargo/config.toml" \
       --replace-fail "@vendor@" "${cargoDeps}"
     cd "$PWD/workspace"
-    cargo llvm-cov
     cargo mutants || mutation_status=$?
     if [ "''${mutation_status:-0}" != 0 ] && [ "''${mutation_status:-0}" != 2 ]; then
       exit "$mutation_status"
