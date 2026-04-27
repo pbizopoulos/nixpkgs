6917f832aaafa112b63f2442b47e895622fb3c6c
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-27 17:25:02 +0300

    .

--- packages/python_latex_template/default.nix
+++ packages/python_latex_template/default.nix
@@ -9,18 +9,18 @@ let
   pythonEnv = pkgs.python313.withPackages (_: pythonDeps);
   runtimeScript = pkgs.writeShellScript "python_latex_template" ''
     set -euo pipefail
-    package_dir="$(cd "$(dirname "$0")/../$(basename "$0")" && pwd)"
+    script_dir="$(cd "$(dirname "$0")" && pwd)"
     rm -rf tmp
-    ${pythonEnv}/bin/python3 "$package_dir/main.py"
-    cp "$package_dir"/ms.{tex,bib} tmp/
+    ${pythonEnv}/bin/python3 "$script_dir/main.py"
+    cp "$script_dir"/ms.{tex,bib} tmp/
     ${pkgs.texliveFull}/bin/latexmk -cd -pdf tmp/ms.tex
   '';
 in
 pkgs.python313Packages.buildPythonPackage rec {
   installPhase = ''
-    install -Dm644 ./main.py $out/${pname}/main.py
-    install -Dm644 ./ms.tex $out/${pname}/ms.tex
-    install -Dm644 ./ms.bib $out/${pname}/ms.bib
+    install -Dm644 ./main.py $out/bin/main.py
+    install -Dm644 ./ms.tex $out/bin/ms.tex
+    install -Dm644 ./ms.bib $out/bin/ms.bib
     install -Dm755 ${runtimeScript} $out/bin/${pname}
   '';
   meta.mainProgram = pname;
