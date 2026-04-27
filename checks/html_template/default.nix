85502aa6f46f70eec21ded0a09ce9fe22e91602e
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-27 23:08:17 +0300

    .

--- checks/html_template/default.nix
+++ checks/html_template/default.nix
@@ -7,8 +7,10 @@ pkgs.testers.runNixOSTest rec {
   name = builtins.baseNameOf ./.;
   nodes.machine.environment.systemPackages = [
     inputs.self.packages.${pkgs.stdenv.system}.${name}
+    pkgs.curl
   ];
   testScript = ''
-    machine.succeed("DEBUG=1 ${name}")
+    machine.succeed("${name} -p 8080 >/tmp/${name}.log 2>&1 &")
+    machine.wait_until_succeeds("curl -fsS http://127.0.0.1:8080 | grep -F 'Hello World!'")
   '';
 }
