5c4ea6dc771e0bd42dce04f481ae326b6685da72
Paschalis Bizopoulos <pbizop@gmail.com>
2026-04-27 17:21:46 +0300

    .

--- packages/python_latex_template/default.nix
+++ packages/python_latex_template/default.nix
@@ -2,7 +2,6 @@
   pkgs ? import <nixpkgs> { },
 }:
 let
-  packageName = builtins.baseNameOf ./.;
   pythonDeps = [
     pkgs.python313Packages.matplotlib
     pkgs.python313Packages.pandas
@@ -10,7 +9,7 @@ let
   pythonEnv = pkgs.python313.withPackages (_: pythonDeps);
   runtimeScript = pkgs.writeShellScript "python_latex_template" ''
     set -euo pipefail
-    package_dir="$(cd "$(dirname "$0")/../${packageName}" && pwd)"
+    package_dir="$(cd "$(dirname "$0")/../$(basename "$0")" && pwd)"
     rm -rf tmp
     ${pythonEnv}/bin/python3 "$package_dir/main.py"
     cp "$package_dir"/ms.{tex,bib} tmp/
@@ -19,13 +18,13 @@ let
 in
 pkgs.python313Packages.buildPythonPackage rec {
   installPhase = ''
-    install -Dm644 ./main.py $out/${packageName}/main.py
-    install -Dm644 ./ms.tex $out/${packageName}/ms.tex
-    install -Dm644 ./ms.bib $out/${packageName}/ms.bib
+    install -Dm644 ./main.py $out/${pname}/main.py
+    install -Dm644 ./ms.tex $out/${pname}/ms.tex
+    install -Dm644 ./ms.bib $out/${pname}/ms.bib
     install -Dm755 ${runtimeScript} $out/bin/${pname}
   '';
   meta.mainProgram = pname;
-  pname = packageName;
+  pname = builtins.baseNameOf src;
   propagatedBuildInputs = pythonDeps;
   pyproject = false;
   src = ./.;
